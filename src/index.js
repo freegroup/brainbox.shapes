d85d96cf43ec9862d27469ada2f679f4470f504b
Herz, Andreas <andreas.herz@sap.com>
2020-03-11 14:20:14 +0100

    no message

--- src/index.js
+++ src/index.js
@@ -3,54 +3,74 @@
  *
  * @type {boilerplate}
  */
-var webPage = require('webpage');
-var fs      = require('fs');
-var system = require('system');
-var page    = webPage.create();
+const webPage = require('webpage');
+const fs = require('fs');
+const system = require('system');
+
+var page = webPage.create();
 var file = system.args[1]
 var shape2CodeDir = system.args[2]
 var shapeDir = system.args[3]
 
 
-function fileToPackage(file){
-  return file
-      .replace(shapeDir,"")
-      .replace(".shape","")
-      .replace(/\//g,"_");
+function fileToPackage(file) {
+    return file
+        .replace(shapeDir, "")
+        .replace(".shape", "")
+        .replace(/\//g, "_");
 }
 
-function shape2CodePath(file){
-  return shape2CodeDir+file;
+function shape2CodePath(file) {
+    return shape2CodeDir + file;
 }
 
+var walk = function(dir) {
+    var results = [];
+    var list = fs.list(dir);
+    list.forEach(function(file) {
+        if(file !== "." && file !== ".."){
+            filePath = dir + '/' + file;
+            if (fs.isDirectory(filePath)) {
+                results = results.concat(walk(filePath));
+            } else {
+                /* Is a file */
+                results.push(fs.absolute(filePath));
+            }
+        }
+    });
+    return results;
+}
 
 function concatFiles(dirname) {
-  var indexFile = dirname+"index.js";
-  var jsonFile = dirname+"index.json";
-  var content = "";
-  var list = [];
-  try{fs.remove(indexFile);}catch(exc){}
-  try{fs.remove(jsonFile);}catch(exc){}
+    var indexFile = dirname + "index.js";
+    var jsonFile = dirname + "index.json";
+    var content = "";
+    var list = [];
+    try {fs.remove(indexFile);} catch (exc) {}
+    try {fs.remove(jsonFile);} catch (exc) {}
 
-  var filenames = fs.list(dirname);
-  filenames.forEach(function(filename) {
-    if (/\.(js)$/.test(filename)) {
-      var name = filename.replace(".js","");
-      var tags = name.split("_");
-      list.push({
-        name:name,
-        tags:tags,
-        filePath:name+".shape"
-      });
-      content += (fs.read(dirname + filename, 'utf-8')+"\n\n\n")
-    }
-  });
-  fs.write(jsonFile,JSON.stringify(list,undefined, 2),"w")
-  fs.write(indexFile,content,"w")
+    walk(dirname).forEach(function (filename) {
+        if (/\.(js)$/.test(filename)) {
+            var relativePath = filename.replace(dirname, "")
+            var basenamePath = relativePath.replace(".js", "")
+            var name = basenamePath.replace(/\//g , "_");
+            var tags = name.split("_");
+            list.push({
+                name: name,
+                tags: tags,
+                filePath: basenamePath + ".shape",
+                image: basenamePath + ".png"
+            });
+            content += (fs.read(filename, 'utf-8') + "\n\n\n")
+        }
+    });
+
+    fs.write(jsonFile, JSON.stringify(list, undefined, 2), "w")
+    fs.write(indexFile, content, "w")
 }
 
 
-function waitFor ($config) {
+function waitFor($config) {
     $config._start = $config._start || new Date();
 
     if ($config.timeout && new Date - $config._start > $config.timeout) {
@@ -67,77 +87,80 @@ function waitFor ($config) {
     setTimeout(waitFor, $config.interval || 0, $config);
 }
 
-page.onConsoleMessage = function(msg) {
+page.onConsoleMessage = function (msg) {
     console.log('CONSOLE: ' + msg);
 };
 
-page.viewportSize = { width: 900, height: 900 };
+page.viewportSize = {width: 900, height: 900};
 
-page.open('http://localhost:7400/designer', function(status) {
+page.open('http://localhost:7400/designer', function (status) {
 
-  console.log(status)
-  if (status === "success") {
-      console.log("Processing: "+file);
-      var json = JSON.parse(fs.read(file));
-      var pkg = fileToPackage(file);
+    console.log(status)
+    if (status === "success") {
+        console.log("Processing: " + file);
+        var json = JSON.parse(fs.read(file));
+        var pkg = fileToPackage(file);
 
-      if(json.draw2d) {
-        json = json.draw2d
-      }
-      json = JSON.stringify(json, undefined,2)
-      var code = fs.read(shape2CodePath("template.js"));
-      fs.write(shape2CodePath("exporter.js"),
-        "var json="+json+";\n"+
-        "var pkg='"+pkg+"';\n"+
-        code);
-      if (page.injectJs(shape2CodePath('exporter.js'))) {
+        if (json.draw2d) {
+            json = json.draw2d
+        }
+        json = JSON.stringify(json, undefined, 2)
+        var code = fs.read(shape2CodePath("template.js"));
+        fs.write(shape2CodePath("exporter.js"),
+            "var json=" + json + ";\n" +
+            "var pkg='" + pkg + "';\n" +
+            code);
         waitFor({
-          check: function () {
-            return page.evaluate(function () {
-              console.log("check img...",img!==null)
-              console.log("check app...",app!==null)
-              return img !== null && app !==null;
-            });
-          },
-          success: function () {
-            try {
-              var jsCode     = page.evaluate(function()  { return code});
-              var customCode = page.evaluate(function () { return customCode; });
-              var markdown   = page.evaluate(function () { return markdown;});
-              var img        = page.evaluate(function () { return img; });
+            check: function () {
+                return page.evaluate(function () {
+                    console.log("check app...", app !== null)
+                    return app !== null;
+                });
+            },
+            success: function () {
+                if (page.injectJs(shape2CodePath('exporter.js'))) {
+                    waitFor({
+                        check: function () {
+                            return page.evaluate(function () {
+                                console.log("check img...", img !== null)
+                                return img !== null;
+                            });
+                        },
+                        success: function () {
+                            try {
+                                var jsCode = page.evaluate(function () { return code });
+                                var customCode = page.evaluate(function () { return customCode; });
+                                var markdown = page.evaluate(function () { return markdown; });
+                                var img = page.evaluate(function () { return img;});
 
-              var pngFilePath      = file.replace(/\.shape$/, ".png");
-              var jsFilePath       = file.replace(/\.shape$/, ".js");
-              var customFilePath   = file.replace(/\.shape$/, ".custom");
-              var markdownFilePath = file.replace(/\.shape$/, ".md");
+                                var pngFilePath = file.replace(/\.shape$/, ".png");
+                                var jsFilePath = file.replace(/\.shape$/, ".js");
+                                var customFilePath = file.replace(/\.shape$/, ".custom");
+                                var markdownFilePath = file.replace(/\.shape$/, ".md");
 
-              // replace the generated "testShape" with the real figure name
-              //
-              console.log("-----",jsCode)
-              jsCode = jsCode.replace(/testShape/g, pkg);
-              customCode = customCode.replace(/testShape/g, pkg);
+                                // replace the generated "testShape" with the real figure name
+                                //
+                                jsCode = jsCode.replace(/testShape/g, pkg);
+                                customCode = customCode.replace(/testShape/g, pkg);
 
-              console.log(jsFilePath)
-              console.log(customFilePath)
-              console.log(markdownFilePath)
-              console.log(pngFilePath)
-              fs.write(jsFilePath, jsCode);
-              fs.write(customFilePath, customCode);
-              fs.write(markdownFilePath, markdown);
-              fs.write(pngFilePath, atob(img), "wb");
+                                fs.write(jsFilePath, jsCode);
+                                fs.write(customFilePath, customCode);
+                                fs.write(markdownFilePath, markdown);
+                                fs.write(pngFilePath, atob(img), "wb");
 
-              concatFiles(shapeDir);
+                                concatFiles(shapeDir);
 
-              phantom.exit(0);
+                                phantom.exit(0);
+                            } catch (exc) {
+                                console.log(exc)
+                            }
+                        },
+                        error: function () {
+                            console.log("error found");
+                        } // optional
+                    });
+                }
             }
-            catch(exc){
-              console.log(exc)
-            }
-          },
-          error: function () {
-            console.log("error found");
-          } // optional
-        });
-      }
+        })
     }
 });
