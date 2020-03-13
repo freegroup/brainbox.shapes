49cc3539919906ab68045ea6dec8c85a47d78667
Andreas Herz <a.herz@freegroup.de>
2020-03-13 23:59:34 +0000

    Generate JS, Documentation and thumbnail from .shape file

--- shapes/circuit/gate/NOR.js
+++ shapes/circuit/gate/NOR.js
@@ -7,7 +7,7 @@
 var circuit_gate_NOR = CircuitFigure.extend({
 
    NAME: "circuit_gate_NOR",
-   VERSION: "1.0.21",
+   VERSION: "1.0.22",
 
    init:function(attr, setter, getter)
    {
--- shapes/circuit/hardware/raspi/RaspiGPIO.js
+++ shapes/circuit/hardware/raspi/RaspiGPIO.js
@@ -7,7 +7,7 @@
 var circuit_hardware_raspi_RaspiGPIO = CircuitFigure.extend({
 
    NAME: "circuit_hardware_raspi_RaspiGPIO",
-   VERSION: "1.0.21",
+   VERSION: "1.0.22",
 
    init:function(attr, setter, getter)
    {
