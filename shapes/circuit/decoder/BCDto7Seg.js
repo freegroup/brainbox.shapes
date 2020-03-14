26dc24d82c5735613884ce28f6459b3962e38863
Andreas Herz <a.herz@freegroup.de>
2020-03-14 12:53:26 +0000

    Generate JS, Documentation and thumbnail from .shape file

--- shapes/circuit/decoder/BCDto7Seg.js
+++ shapes/circuit/decoder/BCDto7Seg.js
@@ -7,7 +7,7 @@
 var circuit_decoder_BCDto7Seg = CircuitFigure.extend({
 
    NAME: "circuit_decoder_BCDto7Seg",
-   VERSION: "1.0.23",
+   VERSION: "1.0.24",
 
    init:function(attr, setter, getter)
    {
--- shapes/circuit/timer/Delay.js
+++ shapes/circuit/timer/Delay.js
@@ -7,7 +7,7 @@
 var circuit_timer_Delay = CircuitFigure.extend({
 
    NAME: "circuit_timer_Delay",
-   VERSION: "1.0.23",
+   VERSION: "1.0.24",
 
    init:function(attr, setter, getter)
    {
