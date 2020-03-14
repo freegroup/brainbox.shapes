7ab87ccf24550bb59e21a554e70c90dba80f4e2f
Andreas Herz <a.herz@freegroup.de>
2020-03-14 00:07:21 +0000

    Generate JS, Documentation and thumbnail from .shape file

--- shapes/circuit/gate/NAND.js
+++ shapes/circuit/gate/NAND.js
@@ -7,7 +7,7 @@
 var circuit_gate_NAND = CircuitFigure.extend({
 
    NAME: "circuit_gate_NAND",
-   VERSION: "1.0.22",
+   VERSION: "1.0.23",
 
    init:function(attr, setter, getter)
    {
--- shapes/circuit/pulse/Delay.js
+++ shapes/circuit/pulse/Delay.js
@@ -7,7 +7,7 @@
 var circuit_pulse_Delay = CircuitFigure.extend({
 
    NAME: "circuit_pulse_Delay",
-   VERSION: "1.0.22",
+   VERSION: "1.0.23",
 
    init:function(attr, setter, getter)
    {
