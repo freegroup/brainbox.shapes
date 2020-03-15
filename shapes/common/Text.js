772304e945cb2d4178aaa023bf77ab2a21992df8
Herz, Andreas <andreas.herz@sap.com>
2020-03-15 22:36:50 +0100

    no message

--- shapes/common/Text.js
+++ shapes/common/Text.js
@@ -3,8 +3,8 @@ var common_Text= draw2d.shape.basic.Text.extend({
     NAME:"common_Text",
     VERSION: "1.0.0",
 
-    init:function(attr)
+    init:function()
     {
-        this._super(attr);
+        this._super({text:"The quick brown fox $ jumps over the lazy dog", bold:false, fontFamily:"Verdana", fontSize:10, bgColor:"#fafafa"});
     }
 });
