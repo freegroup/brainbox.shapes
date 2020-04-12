5d29474b0a5eeca3cd2de1938c6ef4c53c416433
Herz, Andreas <andreas.herz@sap.com>
2020-04-12 23:07:12 +0200

    no message

--- shapes/common/Text.js
+++ shapes/common/Text.js
@@ -1,27 +1,25 @@
-var common_Text= draw2d.shape.basic.Text.extend({
+var common_Text = draw2d.shape.basic.Text.extend({
 
-    NAME:"common_Text",
+    NAME: "common_Text",
     VERSION: "1.0.0",
 
-    init:function()
-    {
-        this._super({ bold:false, fontFamily:"Verdana", fontSize:10, bgColor:"#fafafa"});
+    init: function () {
+        this._super({bold: false, fontFamily: "Verdana", fontSize: 10, bgColor: "#fafafa"});
 
 
-        this.on("change:userData.text", (figure, event)=>{
+        this.on("change:userData.text", (figure, event) => {
             this.setText(event.value)
         })
 
-        this.attr("userData.text","The quick brown fox $ jumps over the lazy dog")
+        this.attr("userData.text", "The quick brown fox $ jumps over the lazy dog")
     },
 
-    getParameterSettings: function()
-    {
+    getParameterSettings: function () {
         return [
             {
-                name:"text",
-                label:"Text",
-                property:{
+                name: "text",
+                label: "Text",
+                property: {
                     type: "longtext"
                 }
 
