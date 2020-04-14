81600d07f7be8b90e61d136d25b15befd01afbba
Andreas Herz <a.herz@freegroup.de>
2020-04-14 14:23:36 +0000

    Generate JS, Documentation and thumbnail from .shape file

--- shapes/circuit/digital/signals/SignalTarget.js
+++ shapes/circuit/digital/signals/SignalTarget.js
@@ -7,7 +7,7 @@
 var circuit_digital_signals_SignalTarget = CircuitFigure.extend({
 
    NAME: "circuit_digital_signals_SignalTarget",
-   VERSION: "1.0.115_203",
+   VERSION: "1.0.116_204",
 
    init:function(attr, setter, getter)
    {
