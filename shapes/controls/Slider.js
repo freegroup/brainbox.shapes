8d46085a4de5d5c5fc28a89550949c668db71172
Herz, Andreas <andreas.herz@sap.com>
2020-05-21 22:03:02 +0200

    no message

--- shapes/controls/Slider.js
+++ shapes/controls/Slider.js
@@ -6,24 +6,33 @@ var controls_Slider = draw2d.shape.widget.Slider.extend({
     init: function () {
         this._super({bold: false, fontFamily: "Verdana", fontSize: 10, bgColor: "#fafafa"});
 
+        this.persistPorts = false
+
+        this.outputPort = new DecoratedOutputPort()
+        this.outputPort.setName("output")
+        this.addPort(this.outputPort)
 
         this.on("change:userData.value", (figure, event) => {
             this.setValue(parseInt(event.value))
         })
 
-        this.attr("userData.value", 4)
+        this.installEditPolicy(new draw2d.policy.figure.AntSelectionFeedbackPolicy());
+
+        this.on("change:value", (element, event) => {
+            var value = parseInt(event.value); // 0..100
+            value = 5 / 100 * value;             // 0..5
+            this.getOutputPort(0).setValue(value);
+        });
+
     },
 
-    calculate: function( context )
-    {
+    calculate: function (context) {
     },
 
-    onStart: function(context)
-    {
+    onStart: function (context) {
     },
 
-    onStop:function(context)
-    {
+    onStop: function (context) {
     },
 
     getParameterSettings: function () {
